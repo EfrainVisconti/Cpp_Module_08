1c7c0b29134620671e9a3f878631b35374ae81c6
Efrain Eduardo Visconti Vivas <eviscont@c4r1s5.42urduliz.com>
2024-11-28 19:17:51 +0100

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 16:10:32 by eviscont          #+#    #+#             */
+/*   Updated: 2024/11/28 16:12:11 by eviscont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+#include <iostream>
+
+const std::string RESET = "\033[0m";
+const std::string GREEN = "\033[32m";
+
+int main()
+{
+	std::cout << GREEN << "-----test-----" << RESET << std::endl;
+
+	return 0;
+}
